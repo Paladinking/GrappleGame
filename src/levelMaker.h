6d03733094e7df16214b24fe557fd0edb36b76aa
Axel Hammarberg <axeha009@student.liu.se>
2022-08-22 17:09:26 +0200

    Started on levelmaker

---
+++ src/levelMaker.h
@@ -0,0 +1,27 @@
+#ifndef LEVEL_MAKER_00_H
+#define LEVEL_MAKER_00_H
+
+
+class LevelMaker : public Game {
+	public:
+		LevelMaker() : Game(640 + 640, 640, "Level maker") {}
+	
+	
+	
+	
+	protected:
+		virtual void handle_mousedown(SDL_MouseButtonEvent e) override {
+			
+		}
+	
+	
+		virtual void init() override {
+			
+			
+		}
+		
+		
+	
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "exceptions.h"
 #include "game.h"
 #include "climbGame.h"
+#include "levelMaker.h"
 
 /**
  * Frees still used global resources and quits SDL and SDL_image.
@@ -32,6 +33,20 @@ void cleanup()
 }
 
 
+/**
+ * Run a game, and set the exit status in case of error.
+ */
+void run_game(Game &game, int* exit_status) {
+	try {
+		game.create();
+		game.run();
+	} catch (base_exception &e) {
+		printf("%s\n", e.msg.c_str());
+		*exit_status = -1;
+	}
+	game.destroy_game();
+}
+
 int main(int argc, char* args[])
 {
 	atexit(cleanup);
@@ -41,17 +56,24 @@ int main(int argc, char* args[])
 		printf("Could not initialize SDL, %s\n", SDL_GetError());
 		exit(-1);
 	}
+	bool level_maker = false;
+	
+	if (argc > 1) {
+		for (unsigned i = 1; i < argc; ++i) {
+			if (args[i] == "level_maker" || args[i] == "--level_maker") {
+				level_maker = true;
+				break;
+			} 
+		}
+	}
 	
 	int exit_status = 0;
-	ClimbGame game;
-	try {
-		game.create();
-		game.run();
-	} catch (base_exception &e) {
-		printf("%s\n", e.msg.c_str());
-		exit_status = -1;
+	if (level_maker) {
+		LevelMaker game;
+		run_game(game, &exit_status);
+	} else {
+		ClimbGame game;
+		run_game(game, &exit_status);
 	}
-	game.destroy_game();
-	
 	return exit_status;
 }
