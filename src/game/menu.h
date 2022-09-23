aa31769d1b8b0e19bcdef6d68ce9ab07e4a2563c
Axel <axel.hammarberg@gmail.com>
2022-09-23 13:02:10 +0200

    Added comments

--- src/game/menu.h
+++ src/game/menu.h
@@ -6,30 +6,57 @@
 
 class Button {
 	public:
+		/**
+		 * Default initialization
+		 */
 		Button() {};
 
+		/**
+		 * Constructs a button with given size and text positioned at given location.
+		 */
 		Button(const int x, const int y, const int w, const int h, const std::string& text);
 
+		/**
+		 * Returns true if the button contains the point (mouseX, mousey). 
+		 */
 		bool is_pressed(const int mouseX, const int mouseY) const;
 
+		/**
+		 * Sets the text of the button to text.
+		 */
 		void set_text(const std::string& text);
 
+		/**
+		 * Sets the position of the button (upper corner) to (x, y).
+		 */
 		void set_position(const int x, const int y);
 
+		/**
+		 * Sets the dimensions of the button to (w, h).
+		 */
 		void set_dimensions(const int w, const int h);
 
+		/**
+		 * Renders the button, looks different if hovered over by the mouse.
+		 */
 		void render(const int mouseX, const int mouseY);
 		
+		/**
+		 * Initializes the button class, loading the font used for the button text.
+		 */
 		static void init();
 	private:
 		int x, y, w, h;
-		
+		//Offset used to center text
 		int text_offset_x, text_offset_y;
 
 		Texture texture;
 
 		std::string text;
-
+		
+		/**
+		 * Generates the texture containing the text of the button. Called by constuctor and set_text.
+		 */
 		void generate_texture();
 		
 		static TTF_Font* font;
@@ -37,29 +64,45 @@ class Button {
 
 class MainMenu : public State {
 	public:
+		/**
+		 * Constructs a MainMenu object.
+		 */
 		MainMenu() : State(SCREEN_WIDTH, SCREEN_HEIGHT, "Climbgame") {};
 
+		/**
+		 * Initializes the MainMenu, creating all buttons.
+		 */
 		virtual void init() override;
 
+		/**
+		 * Handles a down-event of keyboard or mouse.
+		 */
 		virtual void handle_down(const SDL_Keycode key, const Uint8 mouse) override;
 
+		/**
+		 * Handles an up-event of keyboard or mouse.
+		 */
 		virtual void handle_up(const SDL_Keycode key, const Uint8 mouse) override;
 
+		/**
+		 * Ticks the menu, deciding if to switch state.
+		 */
 		virtual void tick(const Uint64 delta, StateStatus& res) override;
 
+		/**
+		 * Renders the full menu.
+		 */
 		virtual void render() override;
 
 	private:
-	
-
 		static const int BUTTON_WIDTH = 180, BUTTON_HEIGHT = 90;
 
 		enum ButtonId {
 			START_GAME, LEVEL_MAKER, OPTIONS, TOTAL, NONE
 		} targeted_button = ButtonId::NONE;
-	
+
 		static const std::string BUTTON_NAMES[ButtonId::TOTAL];
-		
+
 		State* next_state = nullptr;
 
 		std::vector<Button> buttons;
